f68bf1b840068f1e5c28954b5461cfe0ce77e95b
Stas Fomin <stas-fomin@yandex.ru>
2022-05-17 19:00:29 +0300

    -y for pip uninstall

--- install-factory-nuitka.bat
+++ install-factory-nuitka.bat
@@ -1,5 +1,5 @@
 rem #!/bin/sh
 call set-python-path
-python -m pipenv run python -m pip uninstall nuitka
+python -m pipenv run python -m pip uninstall nuitka -y
 python -m pipenv run python C:\projects\nuitka\Nuitka\setup.py install
 rem python -m pipenv install -e git+https://github.com/Nuitka/Nuitka.git@factory#egg=nuitka
